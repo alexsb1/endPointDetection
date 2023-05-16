45fcfabccd90f30a6997f1f6910ffb1d405f3b61
Alex Searle-Barnes <alexsb@live.co.uk>
2023-05-16 16:30:10 +0100

    Higher resolution figures
    Replaced figures with higher resolution versions.
    Replaced "blasts" with "ablated"
    Simplified profile ggplot.

--- R/endPoint.R
+++ R/endPoint.R
@@ -1,4 +1,4 @@
-#' Detects when the laser blasts through the target sample in a laser ablation mass spectrometry time resolved analysis.
+#' Detects when the laser ablates through the target sample in a laser ablation mass spectrometry time resolved analysis.
 #'
 #' This function imports a data frame containing a single time resolved laser ablation mass spectrometry analysis of a foraminifera (or other carbonate shell).
 #' It assumes that the first row contains the signal of the target sample and that background correction has already been applied.
@@ -6,11 +6,11 @@
 #' Column names referencing the time stamps and target signal are specified as function arguments.
 #' Then the signal column is evaluated by smoothing the values using a moving average, then scaled between 0-1 and the rate of change over a number of observations.
 #' \cr
-#' The function identifies the maximum rate of signal change, locates the corresponding time stamp and then subtracts the time it took for the laser to blast through the target.
+#' The function identifies the maximum rate of signal change, locates the corresponding time stamp and then subtracts the time it took for the laser to ablate through the target.
 #' The result (endTime) is the time stamp of the last observation while the laser is still focussed the desired target.
 #'
 #' @param detectDf A data frame containing a single time resolved analysis, with a column referencing time and another with the corresponding measured counts data.
-#' @param dt An integer that controls the number of observations (rows) are used in calculating a rolling lagged difference in 44Ca signal. Using a lower value for a faster blast through of chamber wall can improve end point sensitivity. Default = 10.
+#' @param dt An integer that controls the number of observations (rows) are used in calculating a rolling lagged difference in 44Ca signal. Using a lower value for a faster ablation through of chamber wall can improve end point sensitivity. Default = 10.
 #' @param smoothing Controls the length of the moving average filter to apply over the dt period. Default = 5.
 #' @param timeCol The column title in the data frame identifying the time stamp of the time resolved analysis. Default = "Time".
 #' @param signalCol The column title in the data frame identifying the numerical data used to identify the laser ablation blast through endpoint. This is often 44Ca but could be any column of numerical data that you want to detect the endpoint. Default = "Ca44".
@@ -83,9 +83,9 @@ endPoint <- function(detectDf, dt = 10, smoothing = 5, timeCol = "Time", signalC
     dfReturn$profile <- ggplot2::ggplot(detectDf, ggplot2::aes(x=detectDf[, grep(timeCol, names(detectDf))[1]])) +
       ggplot2::annotate("rect", xmin = startTime - 10, xmax = startTime, ymin = -Inf, ymax = Inf, fill ="red", alpha = 0.5)+ #change -10 to a percentage
       ggplot2::annotate("rect", xmin = endTime, xmax = Inf, ymin = -Inf, ymax = Inf, fill = "red", alpha = 0.5)+
-      ggplot2::geom_point(ggplot2::aes(y=detectDf$Ca44Scaled, colour = "Signal")) +
-      ggplot2::geom_line(ggplot2::aes(y=detectDf$Ca44Scaled, colour = "Signal")) +
-      ggplot2::geom_line(ggplot2::aes(y=detectDf$Ca44dydt, colour = "dydt")) +
+      ggplot2::geom_point(ggplot2::aes(y=Ca44Scaled, colour = "Signal")) +
+      ggplot2::geom_line(ggplot2::aes(y=Ca44Scaled, colour = "Signal")) +
+      ggplot2::geom_line(ggplot2::aes(y=Ca44dydt, colour = "dydt")) +
       ggplot2::geom_vline(xintercept = endTime, colour = "purple")+
       ggplot2::geom_label(x = startTime - 10 , y = stats::median(abs(scale(detectDf[, grep(signalCol, names(detectDf))[1]], center = TRUE))), label = paste("TRA started at", startTime, timeUnits), size = 2, hjust = "left")+
       ggplot2::geom_label(x = endTime, y = mean(abs(scale(detectDf[, grep(signalCol, names(detectDf))[1]], center = TRUE))), label = paste("endTime \n (largest signal change - (dt/scanRate)) \n at", endTime, timeUnits), size = 2)+
